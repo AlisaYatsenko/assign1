e78b69f451b51de44046d925b55cb2515924168d
AlisaYatsenko <ayatsenko@kse.org.ua>
2025-05-20 16:53:30 +0300

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,35 @@
+insert into employees values
+('bob', 'john', 'google'),
+('marina', 'leo', 'amazon'),
+('david', 'nemo', 'google'),
+('george', 'john', 'google'),
+('kylo', 'olena', 'google');
+insert into employers values
+('john', 'google'),
+('ted', 'temu'),
+('nemo', 'google'),
+('olya', 'kyivstar'),
+('olena', 'google');
+insert into salaries values
+(20000, 'bob'),
+(30000, 'marina'),
+(1000, 'david'),
+(16000, 'george'),
+(21000, 'kylo');
+insert into experiences values
+('bob', 2),
+('marina', 6),
+('david', 1),
+('george', 7),
+('kylo', 3),
+('john', 2),
+('ted', 6),
+('nemo', 1),
+('olya', 7),
+('olena', 3);
+insert into favdrink values
+('bob', 'cola'),
+('marina', 'fanta'),
+('david', 'tea'),
+('george', 'coffee'),
+('kylo', 'sprite');
