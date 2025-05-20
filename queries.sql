61328af19a1ae77050f7b2beb6feec207364b416
AlisaYatsenko <ayatsenko@kse.org.ua>
2025-05-21 01:25:04 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,4 @@
+--об'єднуємо поля з п'яти таблиць за іменами за допомогою join
 select e.name, er.name, e.company, s.money, ex.years, f.drink
 from employees e
 join employers er on e.employer=er.name
@@ -5,13 +6,18 @@ join experiences ex on ex.name = e.name
 join salaries s on s.employee = e.name
 join favdrink f on f.name = e.name;
 
+--обираємо всіх employers які не працюють в гугл
 select * from employers where company != 'google';
 
+--сортуємо працівників від найдосвідченішого до найменш досвідченого
 select name, years from experiences
 order by years desc;
 
+--два рядки з таблиці favdrink
 select * from favdrink limit 2;
 
+--створили cte з усіма відомими employees і employers використовуючи union all, бо повторів немає
+--обираємо компанії де працюють більше ніж одна відома людина
 with cte_employ as(
 	select name, company from employees
 	union all
